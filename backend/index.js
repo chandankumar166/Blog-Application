eb371558d1fb4987b701414ffe0e63c7a383f4b4
chandankumar166 <chandanbuyyala21@gmail.com>
2024-03-19 12:42:01 +0530

    Created a database connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log('MongoDB was connected successfully'))
+    .catch((error) => console.log(error));
 
-app.listen(3001, () => console.log('Server is listening'))
+app.listen(3001, () => console.log('Server is listening'));

458594c49625b41e0e67230eee965ea0e78e1a73
chandankumar166 <chandanbuyyala21@gmail.com>
2024-03-19 14:46:22 +0530

    Added error middleware

--- backend/controllers.js/auth.js
+++ backend/controllers.js/auth.js
@@ -1,25 +1,27 @@
 const {signupSchema} = require('../types');
 const {User} = require('../models/user');
 const bcryptjs = require('bcryptjs');
+const {errorHandler} = require('../utils/error');
 
-const signup = async (req, res) => {
-    const {username, email, password} = req.body;
-    const {success} = signupSchema.safeParse(req.body);
-    if (!success) {
-        return res.status(411).json({message: "Incorrect Inputs"});
-    }
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({
-        username,
-        email,
-        password: hashedPassword
-    });
+const signup = async (req, res, next) => {
     try {
+        const {username, email, password} = req.body;
+        const {success} = signupSchema.safeParse(req.body);
+        if (!success) {
+            next(errorHandler(400, 'Incorrect Inputs'));
+        }
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
         await newUser.save();
         return res.status(201).json({message: 'New user got created'});
     }
     catch (error) {
-        return res.status(500).json({message: error.message});
+        next(error);
     }
 
 
--- backend/index.js
+++ backend/index.js
@@ -13,5 +13,14 @@ mongoose.connect(process.env.DATABASE_URL)
 
 app.use(express.json());
 app.use('/api/user', authRouter);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
 
 app.listen(3001, () => console.log('Server is listening'));

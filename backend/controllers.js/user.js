5a371d6dab8d952910a05664404dde8a09d668f9
chandankumar166 <chandanbuyyala21@gmail.com>
2024-03-20 10:21:05 +0530

    Added delete user route

--- backend/controllers.js/user.js
+++ backend/controllers.js/user.js
@@ -1,31 +1,44 @@
 const {User} = require("../models/user");
 const {errorHandler} = require("../utils/error");
-const bcryptjs = require('bcryptjs')
+const bcryptjs = require('bcryptjs');
 
-const update = async (req, res, next) => {
+const updateUser = async (req, res, next) => {
     if (req.user.userId !== req.params.userId) {
-        return next(errorHandler(403, 'You are not the admin'))
+        return next(errorHandler(403, 'You are not the admin'));
     }
     if (req.body?.password < 6) {
-        return next(errorHandler(400, 'Password must be atleast 6 characters.'))
+        return next(errorHandler(400, 'Password must be atleast 6 characters.'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
     try {
         const updatedUser = await User.findByIdAndUpdate({
             _id: req.user.userId
-        },{
+        }, {
             $set: {
                 username: req.username,
                 email: req.body.email,
                 password: req.body.password
             }
-        }, {new: true})
+        }, {new: true});
         const {password, ...rest} = updatedUser._doc;
         return res.status(200).json(rest);
     }
-    catch(error) {
-        next(error)
+    catch (error) {
+        next(error);
     }
-}
+};
 
-module.exports = {update}
+const deleteUser = async (req, res, next) => {
+    if (req.user.userId !== req.params.userId) {
+        return next(errorHandler(403, 'Unauthorized'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        return res.status(200).json({message: 'User has been deleted'});
+    }
+    catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {updateUser, deleteUser};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {verifyUser} = require('../utils/verifyUser');
-const {update} = require('../controllers.js/user');
+const {updateUser, deleteUser} = require('../controllers.js/user');
 
 const userRouter = express.Router();
 
-userRouter.put('/update/:userId',verifyUser, update)
+userRouter.put('/update/:userId',verifyUser, updateUser)
+userRouter.delete('/delete/:userId', verifyUser, deleteUser)
 
 module.exports = {userRouter};

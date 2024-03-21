97bc6a60599cfd953340ca5fbb3c6eb836b57d2a
chandankumar166 <chandanbuyyala21@gmail.com>
2024-03-21 17:32:37 +0530

    Added editComment route

--- backend/controllers.js/comment.js
+++ backend/controllers.js/comment.js
@@ -4,47 +4,47 @@ const {errorHandler} = require("../utils/error");
 
 const createComment = async (req, res, next) => {
     try {
-        const {content, postId, userId}  = req.body;
+        const {content, postId, userId} = req.body;
         const {success} = commentSchema.safeParse(req.body);
         if (!success) {
-            return next(errorHandler(400, 'Incorrect inputs'))
+            return next(errorHandler(400, 'Incorrect inputs'));
         }
         if (userId !== req.user.userId) {
-            return next(errorHandler(403, 'You are not allowed to create the comment'))
+            return next(errorHandler(403, 'You are not allowed to create the comment'));
         }
         const newComment = await Comment.create({
             content,
             postId,
             userId
-        })
+        });
         return res.status(201).json(newComment);
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
 
 const getComments = async (req, res, next) => {
     const postId = req.params.postId;
     try {
-        const comments = await Comment.find({postId: postId}).sort({createdAt: -1})
+        const comments = await Comment.find({postId: postId}).sort({createdAt: -1});
         return res.status(200).json(comments);
     } catch (error) {
         next(error);
     }
-}
+};
 
 const likeComment = async (req, res, next) => {
     try {
         const commentId = req.params.commentId;
         const comment = await Comment.findOne({_id: commentId});
-        
+
         if (!comment) {
             return next(errorHandler(404, 'Comment not found'));
         }
         const userId = req.user.userId;
         const userIndex = comment.likes.findIndex((id) => userId === id);
         if (userIndex === -1) {
-            comment.likes.push(userId)
+            comment.likes.push(userId);
             comment.numberOfLikes += 1;
         }
         else {
@@ -52,11 +52,36 @@ const likeComment = async (req, res, next) => {
             comment.numberOfLikes -= 1;
         }
         await comment.save();
-        return res.status(200).json(comment)
+        return res.status(200).json(comment);
     } catch (error) {
-        next(error)
+        next(error);
     }
 
-}
+};
 
-module.exports = {createComment, getComments, likeComment}
+const editComment = async (req, res, next) => {
+    try {
+        const commentId = req.params.commentId;
+        const userId = req.user.userId;
+        const comment = await Comment.findById({_id: commentId});
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'));
+        }
+        if (comment.userId !== userId && !req.user.isAdmin) {
+            return next(errorHandler(403, 'You are not allowed to edit the comment'));
+        }
+        const editedComment = await Comment.findByIdAndUpdate({_id: commentId},
+            {
+                $set: {
+                    content: req.body.content
+                }
+            }, {new: true}
+        );
+        return res.status(200).json(editedComment)
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+module.exports = {createComment, getComments, likeComment, editComment};
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createComment, getComments, likeComment} = require('../controllers.js/comment');
+const {createComment, getComments, likeComment, editComment} = require('../controllers.js/comment');
 const {verifyUser} = require('../utils/verifyUser');
 
 const commentRouter = express.Router();
@@ -7,5 +7,6 @@ const commentRouter = express.Router();
 commentRouter.post('/create',verifyUser, createComment);
 commentRouter.get('/comments/:postId', getComments)
 commentRouter.put('/likeComment/:commentId', verifyUser, likeComment)
+commentRouter.put('/edit/:commentId',verifyUser, editComment)
 
 module.exports = {commentRouter}

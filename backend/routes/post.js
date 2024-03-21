3eb605b268e716b8d83ce3518377be7f1205b022
chandankumar166 <chandanbuyyala21@gmail.com>
2024-03-21 14:20:40 +0530

    Added getPosts route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {verifyUser} = require('../utils/verifyUser');
-const {createPost} = require('../controllers.js/post');
+const {createPost, getPosts} = require('../controllers.js/post');
 
 const postRouter = express.Router();
 
 postRouter.post('/create', verifyUser, createPost)
+postRouter.get('/posts', getPosts)
 
 module.exports = {postRouter}
